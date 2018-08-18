3ab524929e86c7507226935f81bfc1c94d92cc14
teslatickles <hunterhartline@gmail.com>
2018-08-18 18:47:36 -0500

    revise App.js remove code

--- src/App.js
+++ src/App.js
@@ -9,16 +9,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <header>
           <nav>
             <Link to="/">Landing</Link>
             <Link to="/library">Library</Link>
           </nav>
           <h1>Bloc Jams</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         <main>
           <Route exact path="/" component={Landing} />
           <Route path="/library" component={Library} />
