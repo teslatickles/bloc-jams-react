9e10dda00e647d756cdb7ddd81a82c7de6d3b83c
teslatickles <hunterhartline@gmail.com>
2018-08-22 23:41:24 -0500

    add range input functions

--- src/components/Album.js
+++ src/components/Album.js
@@ -13,8 +13,12 @@ class Album extends Component {
     this.state = {
       album: album,
       currentSong: album.songs[0],
+      currentTime: 0,
+      duration: album.songs[0].duration,
+      volume: 0,
       isPlaying: false,
-      hovered: null
+      hovered: null,
+      volumeDisplay: false
     };
 
     this.audioElement = document.createElement("audio");
@@ -29,6 +33,48 @@ class Album extends Component {
   pause() {
     this.audioElement.pause();
     this.setState({ isPlaying: false });
+  }
+
+  componentDidMount() {
+    this.eventListeners = {
+      timeupdate: e => {
+        this.setState({ currentTime: this.audioElement });
+      },
+      durationchange: e => {
+        this.setState({ duration: this.audioElement.duration });
+      },
+      volumeupdate: e => {
+        this.setState({ volume: this.audioElement.volume });
+      }
+    };
+    this.audioElement.addEventListener(
+      "timeupdate",
+      this.eventListeners.timeupdate
+    );
+    this.audioElement.addEventListener(
+      "durationchange",
+      this.eventListeners.durationchange
+    );
+    this.audioElement.addEventListener(
+      "volumeupdate",
+      this.eventListeners.volumeupdate
+    );
+  }
+
+  componentWillUnmount() {
+    this.audioElement.src = null;
+    this.audioElement.removeEventListener(
+      "timeupdate",
+      this.eventListeners.timeupdate
+    );
+    this.audioElement.removeEventListener(
+      "durationchange",
+      this.eventListeners.durationchange
+    );
+    this.audioElement.removeEventListener(
+      "volumeupdate",
+      this.eventListeners.volumeupdate
+    );
   }
 
   setSong(song) {
@@ -56,6 +102,14 @@ class Album extends Component {
     this.setState({ hovered: null });
   }
 
+  volumeHovered() {
+    this.setState({ volumeDisplay: true });
+  }
+
+  volumeNotHovered() {
+    this.setState({ volumeDisplay: false });
+  }
+
   handlePrevClick() {
     const currentIndex = this.state.album.songs.findIndex(
       song => this.state.currentSong === song
@@ -74,6 +128,38 @@ class Album extends Component {
     const newSong = this.state.album.songs[newIndex];
     this.setSong(newSong);
     this.play();
+  }
+
+  handleTimeChange(e) {
+    const newTime = this.audioElement.duration * e.target.value;
+    this.audioElement.currentTime = newTime;
+    this.setState({ currentTime: newTime });
+  }
+
+  handleVolumeChange(e) {
+    const newVolume = e.target.value;
+    this.audioElement.volume = newVolume;
+    this.setState({ volume: newVolume });
+  }
+
+  formatTime(time) {
+    let minutes, seconds, result;
+    if (time < 60) {
+      minutes = 0;
+      seconds = Math.floor(time);
+    } else if (time > 60) {
+      minutes = Math.floor(time / 60);
+      seconds = Math.floor(time % 60);
+    } else {
+      minutes = "-";
+      seconds = "--";
+    }
+    if (seconds < 10) {
+      result = `${minutes}:0${seconds}`;
+    } else {
+      result = `${minutes}:${seconds}`;
+    }
+    return result;
   }
 
   render() {
@@ -101,7 +187,7 @@ class Album extends Component {
             {this.state.album.songs.map((song, array) => (
               <tr
                 className="song"
-                key={song.title}
+                key={array}
                 onClick={() => this.handleSongClick(song)}
                 onMouseEnter={() => this.handleHover(song)}
                 onMouseLeave={() => this.handleNoHover()}
@@ -132,9 +218,18 @@ class Album extends Component {
         <PlayerBar
           isPlaying={this.state.isPlaying}
           currentSong={this.state.currentSong}
+          currentTime={this.audioElement.currentTime}
+          duration={this.audioElement.duration}
+          formatTime={time => this.formatTime(time)}
+          volume={this.audioElement.volume}
+          volumeHovered={() => this.volumeHovered()}
+          volumeNotHovered={() => this.volumeNotHovered()}
+          volumeDisplay={this.state.volumeDisplay}
           handleSongClick={() => this.handleSongClick(this.state.currentSong)}
           handlePrevClick={() => this.handlePrevClick()}
           handleNextClick={() => this.handleNextClick()}
+          handleTimeChange={e => this.handleTimeChange(e)}
+          handleVolumeChange={e => this.handleVolumeChange(e)}
         />
       </section>
     );
--- src/components/PlayerBar.js
+++ src/components/PlayerBar.js
@@ -18,14 +18,39 @@ class PlayerBar extends Component {
           </button>
         </section>
         <section id="time-control">
-          <div className="current-time">-:--</div>
-          <input type="range" className="seek-bar" value="0" />
-          <div className="total-time">-:--</div>
+          <div className="current-time">
+            {this.props.formatTime(this.props.currentTime)}
+          </div>
+          <input
+            type="range"
+            className="seek-bar"
+            value={this.props.currentTime / this.props.duration || 0}
+            max="1"
+            min="0"
+            step="0.01"
+            onChange={this.props.handleTimeChange}
+          />
+          <div className="total-time">
+            {this.props.formatTime(this.props.duration)}
+          </div>
         </section>
         <section id="volume-control">
           <div className="fas fa-volume-down" />
-          <input type="range" className="seek-bar" value="80" />
+          <input
+            type="range"
+            className="seek-bar"
+            value={this.props.volume || 0}
+            max="1"
+            min="0"
+            step="0.01"
+            onChange={this.props.handleVolumeChange}
+            onMouseEnter={this.props.volumeHovered}
+            onMouseLeave={this.props.volumeNotHovered}
+          />
           <div className="fas fa-volume-up" />
+          {this.props.volumeDisplay
+            ? Math.floor(this.props.volume * 100)
+            : null}
         </section>
       </section>
     );
